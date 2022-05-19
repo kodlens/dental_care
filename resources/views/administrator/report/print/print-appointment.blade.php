fe657745a02c951e167d9ad2b0dbc4473db084ca
etiennewayne <etiennewayne@gmail.com>
2022-05-19 15:45:05 +0800

    update repo

--- resources/views/administrator/report/print/print-appointment.blade.php
+++ resources/views/administrator/report/print/print-appointment.blade.php
@@ -19,6 +19,15 @@
                             <td>{{ $item->user_lname }}, {{ $item->user_fname }} {{ $item->user_mname }}</td>
                             <td>{{ $item->appoint_date }}, {{ $item->appoint_time }} {{ $item->user_mname }}</td>
                             <td>{{ $item->dentist_lname }}, {{ $item->dentist_fname }} {{ $item->dentist_mname }}</td>
+                            <td>
+                                @if($item->appoint_status == 0)
+                                    PENDING
+                                @elseif($item->appoint_status == 1)
+                                    ADMITTED
+                                @else
+                                    CANCELLED
+                                @endif
+                            </td>
                         </tr>
                     @endforeach
                 </tbody>
